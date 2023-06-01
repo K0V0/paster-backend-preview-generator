74259557a048bbc67b685a45d1e1438342b73113
Matej Kováč <matej.kovac@powerflow.cz>
2023-06-01 23:52:48 +0200

    first commit
    (cherry picked from commit c3a3eded1e2bb55983ee2e09272b6c45f12d4b96)

---
+++ src/main/java/com/kovospace/pasterbackendpreviewgenerator/PasterBackendPreviewGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.kovospace.pasterbackendpreviewgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasterBackendPreviewGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasterBackendPreviewGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kovospace/pasterbackendpreviewgenerator/PasterBackendPreviewGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovospace.pasterbackendpreviewgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasterBackendPreviewGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
